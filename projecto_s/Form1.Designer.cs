9bd794979fde24b6c7ed8df9d84462664725cb28
CARLOS JULIO BAYAS CHAVES <carlos.bayas@epn.edu.ec>
2022-02-28 08:59:20 -0500

    Error nombre,apelledo cedula

--- projecto_s/Form1.Designer.cs
+++ projecto_s/Form1.Designer.cs
@@ -66,7 +66,7 @@
             this.lblApellido.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblApellido.Location = new System.Drawing.Point(111, 166);
             this.lblApellido.Name = "lblApellido";
-            this.lblApellido.Size = new System.Drawing.Size(65, 16);
+            this.lblApellido.Size = new System.Drawing.Size(66, 16);
             this.lblApellido.TabIndex = 8;
             this.lblApellido.Text = "Apellido";
             // 
@@ -76,7 +76,7 @@
             this.lblNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblNombre.Location = new System.Drawing.Point(111, 117);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(62, 16);
+            this.lblNombre.Size = new System.Drawing.Size(63, 16);
             this.lblNombre.TabIndex = 9;
             this.lblNombre.Text = "Nombre";
             // 
@@ -93,6 +93,7 @@
             this.txtApellido.Name = "txtApellido";
             this.txtApellido.Size = new System.Drawing.Size(100, 20);
             this.txtApellido.TabIndex = 15;
+            this.txtApellido.TextChanged += new System.EventHandler(this.txtApellido_TextChanged);
             // 
             // pictureBox1
             // 
@@ -121,9 +122,10 @@
             // lblCi
             // 
             this.lblCi.AutoSize = true;
+            this.lblCi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCi.Location = new System.Drawing.Point(156, 212);
             this.lblCi.Name = "lblCi";
-            this.lblCi.Size = new System.Drawing.Size(17, 13);
+            this.lblCi.Size = new System.Drawing.Size(22, 16);
             this.lblCi.TabIndex = 30;
             this.lblCi.Text = "CI";
             // 
@@ -133,6 +135,7 @@
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(100, 20);
             this.txtNombre.TabIndex = 31;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
             // 
             // txtCedula
             // 
@@ -140,6 +143,7 @@
             this.txtCedula.Name = "txtCedula";
             this.txtCedula.Size = new System.Drawing.Size(100, 20);
             this.txtCedula.TabIndex = 32;
+            this.txtCedula.TextChanged += new System.EventHandler(this.txtCedula_TextChanged);
             // 
             // Form1
             // 
